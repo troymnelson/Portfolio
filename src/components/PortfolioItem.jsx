094e5b76e1b2484d61875bb1b0d2f7cdc509f7e3
unknown <troymnlsn@gmail.com>
2023-03-19 16:29:13 -0500

    added assets images working

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 function PortfolioItem({ title, imgUrl, stack, link }) {
     return (
-        <div>
+        <div className='border-2 border-stone-900 rounded-md overflow-hidden'>
             <img 
                 src={imgUrl}
                 alt="portfolio application"
                 className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
             <div className="w-full p-4">
-                <h3>{title}</h3>
-                <p>{stack.map(item => (
-                    <span>
+                <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
+                <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm'>{stack.map(item => (
+                    <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
                         {item}
                     </span>
                 ))}
--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: '3D Forum App',
-        imgUrl: 'minimal-portfolio\src\assets\chatapp.png',
+        imgUrl: '/assets/chatapp.png',
         stack: ['JavaScript', 'Three.js', 'Date-fns', 'Express.js', 'Handlebars', 'bcrypt', 'MySQL', 'Sequelize'],
         link: 'https://github.com/troymnelson/ChatApp',
     },
     {
         title: 'Stock Research App',
-        imgUrl: 'minimal-portfolio\src\assets\ssm.png',
+        imgUrl: '/assets/ssm.png',
         stack: ['Alpha Vantage API', 'iexCloud API', 'jQuery', 'Materialize CSS Framework', 'MomentJS'],
         link: 'https://github.com/troymnelson/superStockMagic',
     },
     {
         title: 'Team Member Generator',
-        imgUrl: 'minimal-portfolio\src\assets\teamMemGen.png',
+        imgUrl: '/assets/teamMemGen.png',
         stack: ['JavaScript', 'Node.js'],
         link: 'https://github.com/troymnelson/teamMemberGenerator',
     },
