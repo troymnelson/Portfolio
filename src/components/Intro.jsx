f7ffda34c44eebfd3b7ee9b986cb40bd04121533
unknown <troymnlsn@gmail.com>
2023-03-19 15:40:33 -0500

    intro section done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,8 +4,12 @@ function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Troy Nelson</h1>
-            <p>Software Engineer and Web Developer</p>
-            <p>This is my biography.</p>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer and Web Developer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">
+                Indie full-stack developer and software engineer looking to make a big impact by working at my goals one step at a time. 
+                Currently pursuing an Associates Degree in Computer Science and recently graduating from a Coding Bootcamp through the University of Minnesota; 
+                I have the skills you need to let your company succeed!
+            </p>
         </div>
     )
 }
