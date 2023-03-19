76a0b7b083dd68e0c4a049c931c475a9512b45f8
unknown <troymnlsn@gmail.com>
2023-03-19 15:14:31 -0500

    common boiler plate

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Intro() {
     return (
-        <div></div>
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Troy Nelson</h1>
+            <p>Software Engineer and Web Developer</p>
+            <p>This is my biography.</p>
+        </div>
     )
 }
 
