7740d688096a58781bfbfbd49d16a6aaa4b48e2e
troymnelson <troymn625@gmail.com>
2025-03-31 22:52:34 -0500

    restart

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 This is my portfolio of web development projects.
 
-It was made using MERN stack and tailwindcss.
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
-app.listen(6969, () => {
-    console.log("App watching on localhost:6969")
-})
